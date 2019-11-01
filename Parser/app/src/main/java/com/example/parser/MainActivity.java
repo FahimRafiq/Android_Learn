310cd9f9f0334566589c0e4c3c57b5f6d09e8b29
Fahim Md Rafiq <fahim6331006@gmail.com>
2019-11-02 02:52:58 +0600

    JSON Parser

--- Parser/app/src/main/java/com/example/parser/MainActivity.java
+++ Parser/app/src/main/java/com/example/parser/MainActivity.java
@@ -3,14 +3,74 @@ package com.example.parser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.print("hi");
+
+        queue = MySingleton.getInstance(this).getRequestQueue();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, "https://jsonplaceholder.typicode.com/todos/1", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("JSON","onResponse"+ response.getString("title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error","oneErrorResponse: "+ error.getMessage());
+            }
+        });
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, "https://jsonplaceholder.typicode.com/todos", null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = new JSONObject();
+                for(int i=0; i<response.length();++i){
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Log.d("JSonArray", "oneArrayRespone: " + jsonObject.getString("title"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Log.d("ArrayError","oneArrayErrorResponse: "+ error.getMessage());
+            }
+        });
+        queue.add(jsonArrayRequest);
+        //requestQueue.add(jsonArrayRequest);
+        //requestQueue.add(jsonObjectRequest);
+
     }
 }
